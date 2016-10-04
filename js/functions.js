5ccf6b20235a0540c9b022e7df4f53c2fd4f5fdd
tabuz <zelek.hubert@gmail.com>
2016-10-04 16:26:54 +0100

    update in work section

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,20 @@
 $(function(){
   smoothScroll(1000);
+  workBelt();
 });
+
+function workBelt(){
+
+  $('.thumb-unit').click(function(){
+    $('.work-belt').css('left','-100%');
+    $('.work-container').show();
+  });
+
+  $('.work-return').click(function(){
+    $('.work-belt').css('left','0%');
+    $('.work-container').hide(800);
+  });
+}
 
 function smoothScroll (duration){
   $('a[href^="#"]').on('click', function(event){
