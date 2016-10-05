369eb2240c12a6626a0ba66dc1ff87b12eab77b1
tabuz <zelek.hubert@gmail.com>
2016-10-05 16:06:24 +0100

    Clients section - add JS, data in *yml

--- js/functions.js
+++ js/functions.js
@@ -2,8 +2,48 @@ $(function(){
   smoothScroll(1000);
   workBelt();
   loadWork();
+  clientStuff();
 });
 
+function clientStuff(){
+  $('.client-unit').first().addClass('active-client');
+  $('.client-logo').first().addClass('active-client');
+
+    $('.client-logo').click(function(){
+      var $this = $(this),
+          $siblings = $this.parent().children(),
+          position = $siblings.index($this);
+
+      $('.client-unit').removeClass('active-client').eq(position).addClass('active-client');
+      $siblings.removeClass('active-client');
+      $this.addClass('active-client');
+    });
+
+    $('.client-control-next, .client-control-prev').click(function(){
+      var $this = $(this),
+          curActiveClient = $('.clients-belt').find('.active-client'),
+          position = $('.clients-belt').children().index(curActiveClient),
+          clientNum = $('.client-unit').length
+
+          if($this.hasClass('client-control-next')){
+            console.log('trre');
+            if(position < clientNum -1){
+              $('.active-client').removeClass('active-client').next().addClass('active-client');
+              }else{
+                $('.client-unit').removeClass('active-client').first().addClass('active-client');
+                $('.client-logo').removeClass('active-client').first().addClass('active-client');
+              }
+            }else{
+                $('.active-client').removeClass('active-client').prev().addClass('active-client');
+                if(position == 0){
+                  $('.client-unit').removeClass('active-client').last().addClass('active-client');
+                  $('.client-logo').removeClass('active-client').last().addClass('active-client');
+                }
+            }
+
+
+    });
+}
 function loadWork(){
   $('.thumb-unit').click(function(){
 
@@ -29,7 +69,6 @@ function workBelt(){
     $('.work-container').hide(800);
   });
 }
-
 function smoothScroll (duration){
   $('a[href^="#"]').on('click', function(event){
 
